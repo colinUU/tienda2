4456ba04b037b2c35218bd5117dfc056363a5226
colinUU <uron_13@outlook.com>
2022-08-02 11:44:10 -0500

    Carpeta borrada

--- compras_productos/src/main/java/com/tiendita/servicio/comprasProductos/models/dao/ComprasProductosDao.java
+++
@@ -1,9 +0,0 @@
-package com.tiendita.servicio.comprasProductos.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tiendita.servicio.comprasProductos.models.entity.CompraProducto;
-
-public interface ComprasProductosDao extends CrudRepository<CompraProducto, Long>{
-
-}
